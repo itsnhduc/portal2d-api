5e403ad551b43f9bd8118566315725d12ad7cf97
sanjanajayakumar <sanjana2902.sj@gmail.com>
2017-03-28 16:08:28 +0800

    add to db

--- server/models/entry.js
+++ server/models/entry.js
@@ -2,4 +2,25 @@
 
 module.exports = function(Entry) {
 
+Entry.addentry = function(name, score, cb) {
+	
+  Entry.create({name : name, score : score}, function(err, entry) {
+      if (err) throw err;
+      console.log('Models created: \n', entry);
+    });
+
+   cb(null, 'Adding… ' + name + '-'+ score);
+  }
+
+  Entry.remoteMethod (
+        'addentry’,{
+          http: {
+		path: '/addentry’, verb: 'get'
+	},
+          accepts: [{arg: 'name', type: 'string'},
+		     {arg: 'score', type: 'number'}],
+          returns: {arg: 'result', type: 'string'}
+        }
+    ); 
+
 };
