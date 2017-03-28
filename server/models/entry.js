ac44be8877a49ae1da30303a40e1a94426002ac9
Duc Nguyen <nhduc.lhp@gmail.com>
2017-03-28 16:30:28 +0800

    Fix string ending spelling

--- server/models/entry.js
+++ server/models/entry.js
@@ -1,26 +1,25 @@
 'use strict';
 
 module.exports = function(Entry) {
-
-Entry.addentry = function(name, score, cb) {
-	
-  Entry.create({name : name, score : score}, function(err, entry) {
+  Entry.addentry = function(name, score, cb) {
+    Entry.create({name: name, score: score}, function(err, entry) {
       if (err) throw err;
       console.log('Models created: \n', entry);
     });
 
-   cb(null, 'Adding… ' + name + '-'+ score);
-  }
+    cb(null, 'Adding… ' + name + '-' + score);
+  };
 
-  Entry.remoteMethod (
-        'addentry’,{
-          http: {
-		path: '/addentry’, verb: 'get'
-	},
-          accepts: [{arg: 'name', type: 'string'},
-		     {arg: 'score', type: 'number'}],
-          returns: {arg: 'result', type: 'string'}
-        }
-    ); 
-
+  Entry.remoteMethod(
+    'addentry', {
+      http: {
+        path: '/addentry', verb: 'get',
+      },
+      accepts: [
+        {arg: 'name', type: 'string'},
+        {arg: 'score', type: 'number'},
+      ],
+      returns: {arg: 'result', type: 'string'},
+    }
+  );
 };
